fca4f40af5aed1e3b10518bb9c764e77ab980fb0
unknown <mutebironaldroninho@gmail.com>
2020-10-13 12:25:25 +0300

    recursive range

---
+++ Recursion_practice/recursiveRange.js
@@ -0,0 +1,7 @@
+/**
+ * Write a function called recursiveRange which accepts a number
+ * and adds up all the numbers from 0 to the number passed to the function.
+ */
+const recursiveRange = (num) => num === 0 ? 0 : num + recursiveRange(num - 1)
+// console.log(recursiveRange(6))   // 21
+// console.log(recursiveRange(5))   // 15
